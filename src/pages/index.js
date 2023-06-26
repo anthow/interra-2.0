321fce910beaccfe7056774af21e8598655093c9
anthow <anthonyenglebert@gmail.com>
2023-06-26 09:24:29 +0200

    /

--- src/pages/index.js
+++ src/pages/index.js
@@ -115,35 +115,6 @@ const IndexPage = ({ data }) => (
       </div>
     </section>
 
-    <article className="w-10/12 m-auto">
-      <section className=" mt-10 flex flex-col space-y-20   m-auto">
-        <div className="flex flex-col gap-10 md:grid grid-cols-2">
-            <figure className="">
-            <GatsbyImage
-              image={data.datoCmsNosProjet.imageDeLaPage.gatsbyImageData}
-              alt={data.datoCmsNosProjet.imageDeLaPage.alt}
-            
-            />
-          </figure>
-          <div className="flex flex-col gap-5">
-
-            <h1 className="font-black text-4xl mb-10 md:mb-20 text-jaune-interra  bg-white text-center"> {data.datoCmsNosProjet.titreDeLaPage}</h1>
-            
-           <div
-            dangerouslySetInnerHTML={{
-              __html: data.datoCmsNosProjet.texteDeLaPage,
-            }}
-            className="paragraphe"
-          ></div>
-                      <a href=" https://miniurl.be/r-4lo4" rel="noreferrer" target="_blank" className="mt-10 text-center"> 
-        <button className=" w-max mt-5 text-white bg-vert-interra font-black  p-1 px-2  rounded hover:bg-white-interra hover:text-vert-interra hover:bg-white border hover:border-vert-interra"> Découvrir nos projets </button>
-
-         </a>
-       
-          </div>
-            </div> 
-    </section>
-    </article>
 
 
     <section className="w-10/12  m-auto">
@@ -303,15 +274,6 @@ export const query = graphql`
       sousMenuAgirAvecNousFaireDon
       sousMenuAgirAvecNousFormerUnduo
       sousMenuAgirAvecNousParticiperFormation
-    }
-    datoCmsNosProjet {
-      titreDeLaPage
-      texteDeLaPage
-      imageDeLaPage {
-        alt
-        gatsbyImageData
-      }
-      urlPdf
     }
   }
 `
--- src/pages/projets.js
+++ src/pages/projets.js
@@ -2,20 +2,65 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Listprojet from "../components/listprojets"
-                                                
+import { GatsbyImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
 
-const projetsPage = () => (
+                                     
+
+const projetsPage = ({data}) => (
     <Layout className="">
            <Seo title="Projets" />
 <article className="< m-auto flex flex-col">
-<h1 className=" text-center font-black text-4xl mb-10 text-vert-interra  bg-white "> Nos projets </h1>
+<article className="w-10/12 m-auto">
+      <section className=" mt-10 flex flex-col space-y-20   m-auto">
+        <div className="flex flex-col gap-10 md:grid grid-cols-2">
+            <figure className="">
+            <GatsbyImage
+              image={data.datoCmsNosProjet.imageDeLaPage.gatsbyImageData}
+              alt={data.datoCmsNosProjet.imageDeLaPage.alt}
+            
+            />
+          </figure>
+          <div className="flex flex-col gap-5">
+
+            <h1 className="font-black text-4xl mb-10 md:mb-20 text-jaune-interra  bg-white text-center"> {data.datoCmsNosProjet.titreDeLaPage}</h1>
+            
+           <div
+            dangerouslySetInnerHTML={{
+              __html: data.datoCmsNosProjet.texteDeLaPage,
+            }}
+            className="paragraphe"
+          ></div>
+                      <a href=" https://miniurl.be/r-4lo4" rel="noreferrer" target="_blank" className="mt-10 text-center"> 
+        <button className=" w-max mt-5 text-white bg-vert-interra font-black  p-1 px-2  rounded hover:bg-white-interra hover:text-vert-interra hover:bg-white border hover:border-vert-interra"> Découvrir nos projets </button>
+
+         </a>
+       
+          </div>
+            </div> 
+    </section>
+    </article>
+
 <Listprojet />
 
 </article>
     </Layout>
 )
 
+export const query = graphql`
+{
+    datoCmsNosProjet {
+        titreDeLaPage
+        texteDeLaPage
+        imageDeLaPage {
+          alt
+          gatsbyImageData
+        }
+        urlPdf
+      }
+    }
 
+`
 
 export default projetsPage
 
